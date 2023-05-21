9919071aeace87402e4fd5e38c137668f82c2592
Kevin <jevringarmindo@gmail.com>
2023-05-21 17:32:37 +0700

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Bot } = require('grammy');
+import { Bot } from 'grammy';
 const token = `6031748891:AAHbDmOP5nK08odbRoDkxb_0OvRKKQ4DHMw`
 
 const bot = new Bot(token);
@@ -7,3 +7,5 @@ bot.on(`message`, async ctx => {
   await ctx.reply(`This bot was deployed on deno.com/deply`)
   return await ctx.reply(`<code>${JSON.stringify(ctx, null, 2)}</code>`, { parse_mode: 'HTML' })
 })
+
+export default bot
--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.186.0/http/server.ts";
 import { webhookCallback } from "https://deno.land/x/grammy@v1.16.0/mod.ts";
 // Kamu mungkin perlu mengubah ini agar dapat melakukan import pada object bot-mu.
-import bot from "./bot.ts";
+import bot from "./index.js";
 
 const handleUpdate = webhookCallback(bot, "std/http");
 
