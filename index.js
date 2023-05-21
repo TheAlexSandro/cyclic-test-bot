4f5ee5fbe57fcf7527420914548bf8bd5fea883c
Kevin <jevringarmindo@gmail.com>
2023-05-21 17:27:55 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-const { UpdateType, TelegramBot } = require('https://deno.land/x/telegram_bot_api/mod.ts');
+const { Bot } = require('grammy');
+const token = `6031748891:AAHbDmOP5nK08odbRoDkxb_0OvRKKQ4DHMw`
 
-const TOKEN = Deno.env.get("TOKEN");
-if (!TOKEN) throw new Error("Bot token is not provided");
-const bot = new TelegramBot(TOKEN);
+const bot = new Bot(token);
 
-bot.on(UpdateType.Message, async ({ ctx }) => {
-  var chatID = ctx.chat.id;
-
-  await bot.sendMessage(`This bot was deployed on deno.com/deploy`)
-  return await bot.sendMessage(chatID, `<code>${JSON.stringify(ctx, null, 2)}</code>`, { parse_mode: 'HTML' })
-});
-
-bot.run({
-  polling: true,
-});
+bot.on(`message`, async ctx => {
+  await ctx.reply(`This bot was deployed on deno.com/deply`)
+  return await ctx.reply(`<code>${JSON.stringify(ctx, null, 2)}</code>`, { parse_mode: 'HTML' })
+})
---
+++ mod.js
@@ -0,0 +1,20 @@
+const { serve } = require('https://deno.land/std@0.186.0/http/server.ts');
+const { webhookCallback } = require('https://deno.land/x/grammy@v1.16.0/mod.ts');
+// Kamu mungkin perlu mengubah ini agar dapat melakukan import pada object bot-mu.
+const bot = require('./index.js')
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+serve(async (req) => {
+  if (req.method === "POST") {
+    const url = new URL(req.url);
+    if (url.pathname.slice(1) === bot.token) {
+      try {
+        return await handleUpdate(req);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
+  return new Response();
+});
